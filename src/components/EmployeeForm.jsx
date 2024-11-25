8a3f3ea9000a94aba8c6779b7263aac916ed572d
Damien <damien.dev2022@gmail.com>
2024-11-25 16:18:52 +0100

    changement controler vers non controler

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -11,88 +11,13 @@ import {
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { departments, initialFormState, usStates } from "../constants/formData";
-import { addEmployee } from "../redux/slice/employeeSlice";
+import { departments, usStates } from "../constants/formData";
 import theme from "../styles/theme";
 
 const EmployeeForm = () => {
-  const dispatch = useDispatch();
-  const [error, setError] = useState({
-    startDate: "",
-    dateOfBirth: "",
-    state: "",
-    department: "",
-  });
-
-  const [formData, setFormData] = useState(initialFormState);
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const validate = (fieldName, value) => {
-    const newErrors = {};
-
-    if (!value) {
-      newErrors[fieldName] = fieldName + "is required";
-    }
-    return newErrors;
-  };
+  // const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const error = {};
-
-    // Verification des champs requis
-    // if (!formData.dateOfBirth) {
-    //   newErrors.dateOfBirth = "Date Of Birth is required";
-    // }
-    // if (!formData.startDate) {
-    //   newErrors.startDate = "Start date is required";
-    // }
-    // if (!formData.state) {
-    //   newErrors.state = "State is required";
-    // }
-    // if (!formData.department) {
-    //   newErrors.department = "Department is required";
-    // }
-    const departmentErrors = validate("department", formData.department);
-
-    // Si des erreurs existent, les enregistrer et stopper la soumission
-    Object.assign(error, departmentErrors);
-    if (Object.keys(error).length > 0) {
-      return setError(error);
-    } else {
-      setError({});
-      console.log("Ajout employé");
-    }
-
-    const dateOfBirthObj = formData.dateOfBirth
-      ? formData.dateOfBirth.toISOString()
-      : null;
-
-    const startDateObj = formData.startDate
-      ? formData.startDate.toISOString()
-      : null;
-
-    const newEmployee = {
-      ...formData,
-      dateOfBirth: dateOfBirthObj,
-      startDate: startDateObj,
-    };
-    dispatch(addEmployee(newEmployee));
-    console.log("Employé ajouté:", newEmployee);
-    handleReset();
-  };
-
-  const handleReset = () => {
-    setFormData(initialFormState);
   };
 
   return (
@@ -115,112 +40,29 @@ const EmployeeForm = () => {
             <form onSubmit={handleSubmit}>
               <Stack spacing={3}>
                 <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-                  <TextField
-                    fullWidth
-                    label="First Name"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                  />
-                  <TextField
-                    fullWidth
-                    label="Last Name"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                  />
+                  <TextField fullWidth label="First Name" name="firstName" />
+                  <TextField fullWidth label="Last Name" name="lastName" />
                 </Stack>
 
                 <Stack direction={{ xs: "column", sm: "row" }} spacing={3}>
-                  <DatePicker
-                    label="Date of birth"
-                    value={formData.dateOfBirth}
-                    onChange={(newValue) => {
-                      setFormData((prev) => ({
-                        ...prev,
-                        dateOfBirth: newValue,
-                      }));
-                      setError(!newValue);
-                    }}
-                    slotProps={{
-                      textField: {
-                        error: !!error.dateOfBirth,
-                        helperText: error.dateOfBirth || "",
-                      },
-                    }}
-                    sx={{ width: "100%" }}
-                  />
+                  <DatePicker label="Date of birth" sx={{ width: "100%" }} />
 
-                  <DatePicker
-                    label="Start Date"
-                    value={formData.startDate}
-                    onChange={(newValue) => {
-                      setFormData((prev) => ({
-                        ...prev,
-                        startDate: newValue,
-                      }));
-                      setError(!newValue);
-                    }}
-                    slotProps={{
-                      textField: {
-                        error: !!error.startDate,
-                        helperText: error.startDate || "",
-                      },
-                    }}
-                    sx={{ width: "100%" }}
-                  />
+                  <DatePicker label="Start Date" sx={{ width: "100%" }} />
                 </Stack>
 
                 <Typography variant="h6" sx={{ mt: 2, color: "#5f6f1f" }}>
                   Adresse
                 </Typography>
 
-                <TextField
-                  fullWidth
-                  label="Street"
-                  name="street"
-                  value={formData.street}
-                  onChange={handleChange}
-                />
+                <TextField fullWidth label="Street" name="street" />
 
                 <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-                  <TextField
-                    fullWidth
-                    label="City"
-                    name="city"
-                    value={formData.city}
-                    onChange={handleChange}
-                  />
+                  <TextField fullWidth label="City" name="city" />
 
-                  <TextField
-                    fullWidth
-                    label="Zip Code"
-                    name="zipCode"
-                    value={formData.zipCode}
-                    onChange={handleChange}
-                  />
+                  <TextField fullWidth label="Zip Code" name="zipCode" />
                 </Stack>
                 <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-                  <TextField
-                    fullWidth
-                    select
-                    label="State"
-                    name="state"
-                    value={formData.state}
-                    onChange={handleChange}
-                    error={!!error.state}
-                    helperText={error.state}
-                    SelectProps={{
-                      MenuProps: {
-                        PaperProps: {
-                          sx: {
-                            maxHeight: 200,
-                            overflowY: "auto",
-                          },
-                        },
-                      },
-                    }}
-                  >
+                  <TextField fullWidth select label="State" name="state">
                     {usStates.map((states) => (
                       <MenuItem key={states.value} value={states.name}>
                         {states.name}
@@ -232,10 +74,6 @@ const EmployeeForm = () => {
                     select
                     label="Départment"
                     name="department"
-                    value={formData.department}
-                    onChange={handleChange}
-                    error={!!error.department}
-                    helperText={error.department}
                   >
                     {departments.map((dept) => (
                       <MenuItem key={dept.id} value={dept.name}>
@@ -249,13 +87,6 @@ const EmployeeForm = () => {
                   spacing={2}
                   sx={{ justifyContent: "flex-end", mt: 4 }}
                 >
-                  <Button
-                    variant="outlined"
-                    onClick={handleReset}
-                    sx={{ px: 4 }}
-                  >
-                    Reset
-                  </Button>
                   <Button type="submit" variant="contained" sx={{ px: 4 }}>
                     Submit
                   </Button>
